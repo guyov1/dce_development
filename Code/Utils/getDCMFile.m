0c25f2c5f1f9c38b95e1ded2ec5d42ceadebf180
Guy Nadav <guyov1@gmail.com>
2015-01-10 11:47:38 +0200

    Adding the option to read .dcm that start with "MR1"

--- Code/Utils/getDCMFile.m
+++ Code/Utils/getDCMFile.m
@@ -4,6 +4,9 @@ if(isempty(DD))
     DD=dir([in filesep '00*.dcm']);
 end
 if(isempty(DD))
+    DD=dir([in filesep 'MR1*.dcm']);
+end
+if(isempty(DD))
     Out=[];
 else
     Out=[in filesep DD(2).name];
